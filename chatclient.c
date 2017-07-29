aa63be6d768ab86779d693255a9858f66738a60c
daveyan <idontwanttotalk@gmail.com>
2017-07-29 17:28:49 -0400

    inital chatclient

---
+++ chatclient.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <sys/types.h>
+
+int main(int argc, char*argv[]){
+	if (argc !=3){
+		printf("Usage: ./chatclient [host name] [port number]");
+		exit(1);
+	}
+
+	server
+}
