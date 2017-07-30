b7d1708751161a549f64dd7d0667e24ae315fee4
daveyan <idontwanttotalk@gmail.com>
2017-07-30 14:38:09 -0400

    comments added

--- chatclient.c
+++ chatclient.c
@@ -10,6 +10,7 @@
 #include <unistd.h> 
 
 int main(int argc, char*argv[]){
+	//check to see if we have the appropriate arguments
 	if (argc !=3){
 		printf("Usage: ./chatclient [host name] [port number]");
 		exit(1);
@@ -17,27 +18,30 @@ int main(int argc, char*argv[]){
 
 	char * hostname;
 	char * port;
-
+	//set augments to the correct vars
 	hostname = argv[1];
 	port = argv[2];
 
 	struct addrinfo hints, *result;
-
+	//reset the hints to 0 following the man pages for get addrinfo
+	//http://man7.org/linux/man-pages/man3/getaddrinfo.3.html
 	memset(&hints,0,sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC; 
 	hints.ai_socktype = SOCK_STREAM;
 
-	//http://man7.org/linux/man-pages/man3/getaddrinfo.3.html
+	
 	getaddrinfo(hostname,port,&hints,&result);
 
 	int sockfd;
 	int socket_connect;
+	//socket creation
 	sockfd = socket(result->ai_family,result->ai_socktype, result->ai_protocol);
 	if(sockfd == -1){
 		printf("Error creating socket\n");
 		close(sockfd);
 		exit(1);
 	}
+	//socket connection
 	socket_connect = connect(sockfd,result->ai_addr,result->ai_addrlen);
 	if(socket_connect == -1){
 		printf("Error connecting to server\n");
@@ -51,17 +55,21 @@ int main(int argc, char*argv[]){
 	int client_length = 0;
 	size_t input_length = 11;
 
+	//setting the client handle
 	client_handle = malloc(sizeof(char)* 11);
 	printf ("Please enter a client handle [10 characters or less]\n");
 	
 	while(client_length > 10 || client_length == 0){
 		client_length = getline(&client_handle,&input_length,stdin);
+		//remove the new line character at the end of the getline
 		client_handle[strcspn(client_handle, "\n")] = 0;
 
 	}
 	
+	//send the client handle to the server
 	send(sockfd,client_handle,strlen(client_handle),0);
 
+	//recieve the server handle from server
 	char* server_handle;
 	server_handle = malloc(sizeof(char)*11);
 	recv(sockfd,server_handle,input_length,0);
@@ -69,6 +77,7 @@ int main(int argc, char*argv[]){
 	//printf("%s\n",server_handle);
 	fflush(stdout);
 
+	//read and write buffers	
 	char read_buff[512];
 	char write_buff[512];
 
@@ -81,15 +90,13 @@ int main(int argc, char*argv[]){
         fflush(stdout);
         printf("%s> ",client_handle);
         fgets(write_buff, 512, stdin);       
-        //check for quit signal
+        //check for the quit command
         if (strncmp(write_buff, "\\quit", 5) == 0){
-
             printf("Quitting chat\n");
             quit = 0;
         }
         //write message to server
         int send_test = 0;
-
      	send_test = send(sockfd, write_buff, strlen(write_buff), 0);
      	if(send_test == -1){
      		printf("Connection is closed");
@@ -97,8 +104,7 @@ int main(int argc, char*argv[]){
      		exit(0);
      	}
      
-                
-        
+        //readint the response from the server.        
         memset(&read_buff, 0, sizeof(read_buff));
         recv(sockfd,read_buff, 512, 0);
         printf("%s> %s\n", server_handle, read_buff);
@@ -106,7 +112,8 @@ int main(int argc, char*argv[]){
         fflush(stdout);
 
 	}
-	
+	//client has enter quit
+	//close the connection
 	close(sockfd);
 	printf("Closing connection to server");
 	return 0;
--- chatserve.py
+++ chatserve.py
@@ -9,52 +9,74 @@ if len(sys.argv) !=2:
 	print("Usage: ./chatserve.py [port number] ")
 	sys.exit(1)
 
-#empty server handle string
+#empty server handle string and get handle from server user
 server_handle = ''
 while(len(server_handle) > 10 or len(server_handle) == 0):
 	server_handle = input("Enter a server handle (10 characters or less)\n")
 
 
-def sendMessages(connectionSocket):
-	while True:
-		server_input = input(server_handle + "> ")
-		server_message = server_handle+"> " + input +'\0'
-		connectionSocket.send(server_message)
+def sendMessages(connectionSocket,message):
+	totalsent = 0
+	#send over the message one char at a time until we reach the buffer length
+	while totalsent < len(message):
+		#encode the byte to str
+		sent = connectionSocket.send(message[totalsent:].encode())
+		if sent == 0:			
+			connectionSocket.close()
+		totalsent += sent
 
 
 
 def receiveMessages(connectionSocket):
 	while True:
+		#start with an empty string
+		client_message = ''
 		client_message = connectionSocket.recv(512)
+		#return a decoded version
 		return client_message.decode()
 
-
 port = int(sys.argv[1])
+
+#create and bind the socket
 server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server_socket.bind(('',port))
+
+#listen for new requests
 server_socket.listen(1)
 
 print('The server is now listening for a connection')
 
 while True:
-	connectionSocket, address = server_socket.accept()
-	print("connection made from address {}".format(address))
+	connectionSocket, addr = server_socket.accept()
 
+	#read the client handle from the socket
 	client_handle = receiveMessages(connectionSocket)
+	print(client_handle +" has connected to the server\n")
 
+	#use for all returning responses
+	client_prepend = client_handle + "> "
+			
+	#send handle to client
 	connectionSocket.send(server_handle.encode())
+	#use for all server responses
+	server_prepend = server_handle + "> "
 
-	while True:
-		receieved_message = connectionSocket.recv(512)
-		if receieved_message:
-			print(client_handle+"> "+receieved_message.decode())
-
-		send_message = input(server_handle + "> ")
-		if send_message == "\quit":
-			print("Quitting chat")
+	while 1:
+		#receive message
+		message = receiveMessages(connectionSocket)
+		if message:
+			message = message.rstrip()
+			print (client_prepend + message)
+		
+		#send message
+		sentmessage = input(server_prepend)
+		if sentmessage == '\quit':
+			print ("Quitting chat")
 			break
-		else:
-			sendMessages(connectionSocket)
+		sendMessages(connectionSocket, sentmessage)
+		
+		#close connection
 	connectionSocket.close()
-	print("Connection is now closed")
+	print ("Connection is now closed")
+	sys.exit(0)
 
